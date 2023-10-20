a932ce57c99e2f252b929c4168a46af959c4ce37
Oli Hock <oliver.hock@gmail.com>
2023-10-20 20:38:22 +0200

    Add data_import.html template and upload route, allowing users to import and upload files.

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 import logging
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_oidc import OpenIDConnect
+from werkzeug.utils import secure_filename
 
 load_dotenv()
+base_url = os.environ['APP_BASE_URL']
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,6 +21,8 @@ app.config.update({
     'OIDC_USER_INFO_ENABLED': True,
     'OIDC_OPENID_REALM': os.environ['FLASK_OIDC_OPENID_REALM'],
 })
+
+os.makedirs(os.path.join(app.instance_path, 'upload_files'), exist_ok=True)
 
 oidc = OpenIDConnect(app)
 
@@ -39,6 +43,21 @@ def profile():
     info = oidc.user_getinfo(['email', 'openid_id'])
     return ('Hello, %s (%s)! <a href="/">Return</a>' %
             (info.get('email'), info.get('openid_id')))
+
+
+@app.route('/data_import')
+@oidc.require_login
+def data_import():
+    email = oidc.user_getfield('email')
+    return render_template('data_import.html', base_url=base_url)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join(app.instance_path, 'upload_files', secure_filename(file.filename)))
+        return 'File uploaded successfully'
 
 
 @app.route('/logout')
