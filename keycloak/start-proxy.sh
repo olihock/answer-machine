861b95d3af49b6c370107ceb983b104740494bbe
Oliver Hock <oliver.hock@gmail.com>
2023-12-05 11:43:26 +0000

    restart containers always

--- keycloak/start-proxy.sh
+++ keycloak/start-proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export KEYCLOAK_COMMAND="start --proxy edge --hostname=localhost --hostname-debug=true --hostname-strict=true --http-relative-path=/keycloak"
+export KEYCLOAK_COMMAND="start --proxy edge --hostname=integ.dynv6.net --hostname-debug=true --hostname-strict=true --http-relative-path=/keycloak"
 docker compose up -d
